49099f28518e61e0f0ccab3aeaba06098f9c034b
Praise W <praisewuche69@gmail.com>
2024-09-24 13:53:33 +0100

    just created the game board

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,93 @@
-import { useState } from 'react'
+import { useState } from "react";
 function App() {
-
   return (
     <>
-      <div className='Board'>
+      <div className="screen">
+        <div className="Board">
+          <div className="row">
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+          </div>
+          <div className="row">
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+          </div>
+          <div className="row">
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+          </div>
+          <div className="row">
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+          </div>
+          <div className="row">
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+          </div>
+          <div className="row">
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+          </div>
+          <div className="row">
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+          </div>
+          <div className="row">
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+            <div className="box white"></div>
+            <div className="box black"></div>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
